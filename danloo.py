a3dae33c7aa4d43ec2cb7d0cbeecd96bea32ec75
Daniel <113395541+dan1oo@users.noreply.github.com>
2023-01-22 18:27:46 -0600

    Update danloo.py

--- danloo.py
+++ danloo.py
@@ -1,24 +1,18 @@
-from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
+from adafruit_hid.keycode import Keycode 
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 app = {'title' : 'Danlooo', # Application name
-    'macros' : [
-        (0x9e0129, 'Console', [',']),
-        (0x9e0129, 'Clip', ([Keycode.ALT,'1'])),
-        (0x9e0129, 'Mute', ['`']),
-        # 2nd row ----------
-        (0x9e0129, 'Alt tab', [Keycode.ALT, Keycode.TAB]),
-        (0x9e0129, 'PW', ['Look/aturphonebruh7']),
-        (0x9e0129, 'UT PW', ['Look.aturphonebruh7']),
-        # 3rd row ----------
-        (0x9e0129, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),
-        (0x29e0129, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
-        (0x9e0129, 'AB', [Keycode.F3]),
-        # 4th row ----------
-        (0x9e0129,  '<<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),
-        (0x9e0129, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),
-        (0x9e0129, '>>', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
-     
+        'macros' : [ #Each row corresponds to respective row and keys on the hardware
+                     #Consumer control codes control media commands, Keycodes control nonstring keys.
+       
+        (0x9e0129, 'Console', [',']), (0x9e0129, 'Clip', ([Keycode.ALT,'1'])), (0x9e0129, 'Mute', ['`']),
+        
+        (0x9e0129, 'Alt tab', [Keycode.ALT, Keycode.TAB]), (0x9e0129, 'PW', ['Sample']), (0x9e0129, 'UT PW', ['Sample']),
+       
+        (0x9e0129, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),(0x29e0129, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),(0x9e0129, 'AB', [Keycode.F3]),
+       
+        (0x9e0129,  '<<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),(0x9e0129, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),(0x9e0129, '>>', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
+        # Digital Encoder
         (0x9e0129, '', [Keycode.BACKSPACE])
     ]
 }
